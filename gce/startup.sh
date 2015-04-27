d68c67b8f9ce7f4b5cff0b382f8ff3ce713c9fb0
Mark Ethan Trostler <mark@zzo.com>
2015-04-26 21:47:33 -0700

    install nodejs

--- gce/startup.sh
+++ gce/startup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 apt-get update
 curl -sL https://deb.nodesource.com/setup | sudo bash -
-apt-get install -y git npm libwww-perl build-essential
+apt-get install -y git nodejs libwww-perl build-essential
 git clone https://github.com/zzo/sivart
 cd sivart
 npm install
