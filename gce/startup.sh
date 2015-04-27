7b73dc9d1cf5f3f82c986bb10163dd72955da77d
Mark Ethan Trostler <mark@zzo.com>
2015-04-26 21:40:05 -0700

    get npm

--- gce/startup.sh
+++ gce/startup.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 apt-get update
-apt-get install -y git npm libwww-perl
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+apt-get install -y git npm libwww-perl build-essential
 git clone https://github.com/zzo/sivart
 cd sivart
 npm install
