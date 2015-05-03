862895b42a350bd0092d36476224f09b29806327
Mark Ethan Trostler <mark@zzo.com>
2015-05-03 08:05:58 -0700

    accept old push

--- gce/create_sivart_master.js
+++ gce/create_sivart_master.js
@@ -4,13 +4,16 @@ var projectId = 'focal-inquiry-92622';
 var sivart_master = new Ins(projectId, 'us-central1-a', 'sivart-master');
 
 sivart_master.create({ file: 'sivart-master.json' }, function(err, resp) {
-  console.log('created master:');
-  console.log(resp);
-  sivart_master.tail_gce_console(function(err, data) {
-    console.log(data);
-    if (data.toString().match('__ALIVE__')) {
-      return true;
-    }
-  });
+  if (err) {
+    console.error('Error creating master');
+    console.error(err);
+  } else {
+    sivart_master.tail_gce_console(function(err, data) {
+      console.log(data);
+      if (data.toString().match('__ALIVE__')) {
+        return true;
+      }
+    });
+  }
 });
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var Server = require('./lib/Server');
 var Callbacks = require('./lib/Callbacks');
+var util = require('./lib/util');
 var GCE = require('sivart-GCE');
 
 var port = process.argv[2] || 8080;
@@ -8,7 +9,7 @@ var zone = 'us-central1-a';
 
 var gce = new GCE(projectId, zone);
 var callbacks = new Callbacks(gce, { });
-var server = new Server(callbacks);
+var server = new Server(callbacks, util);
 
 gce.start(function() {
   server.start(port, function(s) {
