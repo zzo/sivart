fce26a97ba0d22d58f4c3296c4a085c24a4f9de5
Mark Ethan Trostler <mark@zzo.com>
2015-05-03 08:10:12 -0700

    get rid of util

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var Server = require('./lib/Server');
 var Callbacks = require('./lib/Callbacks');
-var util = require('./lib/util');
 var GCE = require('sivart-GCE');
 
 var port = process.argv[2] || 8080;
@@ -9,7 +8,7 @@ var zone = 'us-central1-a';
 
 var gce = new GCE(projectId, zone);
 var callbacks = new Callbacks(gce, { });
-var server = new Server(callbacks, util);
+var server = new Server(callbacks);
 
 gce.start(function() {
   server.start(port, function(s) {
