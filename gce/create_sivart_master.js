3993e55ce63d01c0185abcb3836ee9942f918095
Mark Ethan Trostler <mark@zzo.com>
2015-05-03 23:33:42 -0700

    delete master first

--- gce/create_sivart_master.js
+++ gce/create_sivart_master.js
@@ -1,19 +1,20 @@
 var Ins = require('sivart-GCE/Instance');
-var projectId = 'focal-inquiry-92622';
+var Auth = require('sivart-GCE/Auth');
 
-var sivart_master = new Ins(projectId, 'us-central1-a', 'sivart-master');
+var sivart_master = new Ins(Auth.projectId, 'us-central1-a', 'sivart-master');
 
-sivart_master.create({ file: 'sivart-master.json' }, function(err, resp) {
-  if (err) {
-    console.error('Error creating master');
-    console.error(err);
-  } else {
-    sivart_master.tail_gce_console(function(err, data) {
-      console.log(data);
-      if (data.toString().match('__ALIVE__')) {
-        return true;
-      }
-    });
-  }
+sivart_master.delete(function(err, resp) {
+  sivart_master.create({ file: 'sivart-master.json' }, function(err, resp) {
+    if (err) {
+      console.error('Error creating master');
+      console.error(err);
+    } else {
+      sivart_master.tail_gce_console(function(err, data) {
+        console.log(data);
+        if (data.toString().match('__ALIVE__')) {
+          return true;
+        }
+      });
+    }
+  });
 });
-
